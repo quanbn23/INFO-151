a200aef7767ba3d187459e60c46b4c776d1759d9
quanbn23 <qbn23@drexel.edu>
2023-11-29 21:39:32 -0500

    week 8/9 sign up modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
+//show and hide navigations
 var nav = document.querySelector("#navigations");
 var signInContainer = document.querySelector("#buttons-container");
 var soccerBtn = document.querySelector("#soccerBall button");
+
+var soccerBall = document.querySelector(".fa-futbol");
+var xBtn = document.querySelector(".fa-x");
 
 var mainNavVisible = false;
 
@@ -9,6 +13,7 @@ function showNav() {
     nav.classList.add("showNavs");
     signInContainer.classList.add("showButtons");
     mainNavVisible = true;
+    xBtn.classList.add("hide-soccerball");
   } else {
     nav.classList.remove("showNavs");
     signInContainer.classList.remove("showButtons");
@@ -17,3 +22,21 @@ function showNav() {
 }
 
 soccerBtn.addEventListener("click", showNav);
+
+//show and hide sign up modal
+var signUpBtn = document.querySelector("#sign-up-btn");
+var signUpContainer = document.querySelector("#sign-up-container");
+var closeSignUpModalBtn = document.querySelector("#close-sign-up-modal-btn");
+
+function showSignUpModal() {
+  signUpContainer.classList.add("show-sign-up-container");
+  nav.classList.remove("showNavs");
+  signInContainer.classList.remove("showButtons");
+}
+
+function hideSignUpModal() {
+  signUpContainer.classList.remove("show-sign-up-container");
+}
+
+signUpBtn.addEventListener("click", showSignUpModal);
+closeSignUpModalBtn.addEventListener("click", hideSignUpModal);
